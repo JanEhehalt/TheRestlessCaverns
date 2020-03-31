965c815f1f587d33cd633f157a8fdd43c43c10f5
GammelJan <jan.ehehalt@gmail.com>
2020-03-31 23:08:29 +0200

    Inventory Grundmethoden

---
+++ core/src/com/dungeoncrawler/Inventory.java
@@ -0,0 +1,37 @@
+package com.dungeoncrawler;
+
+
+public class Inventory {
+    
+    Item items[][];
+    
+    
+    Inventory(int width, int height){
+        items = new Item[width][height];
+    }
+    
+    public void addItem(Item i)
+    {
+        
+        for(int n = 0; n <= items.length; n++){
+            for(int m = 0; m <= items[0].length; m++){
+                if(items[n][m] == null){
+                    items[n][m] = i;
+                    n = items.length + 1;
+                    break;
+                } 
+            }
+        }
+    }
+    
+    public void dropItem(int x, int y){
+        items[x][y] = null;
+    }
+    
+    
+    public Item getItem(int x, int y){
+        return items[x][y];
+    }
+    
+    
+}
