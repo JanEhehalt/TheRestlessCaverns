8c1b5b9d88bbb8acb4fa927e1c5c43b0edbdd283
GammelJan <jan.ehehalt@gmail.com>
2020-04-01 11:46:03 +0200

    Hauptmenüerstellung beginnnt

--- core/src/com/dungeoncrawler/Main.java
+++ core/src/com/dungeoncrawler/Main.java
@@ -1,7 +1,14 @@
 package com.dungeoncrawler;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.dungeoncrawler.view.View;
 
 public class Main extends ApplicationAdapter {
 	
+    View v;
+    
+    Main(){
+        v = new View();
+    }
+    
 }
