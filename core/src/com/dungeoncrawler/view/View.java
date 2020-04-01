eb07ed407daec730a00c71880482b211829096a6
Jonathan Hager <jonathan.hager@protonmail.com>
2020-04-01 20:29:15 +0200

    Map crasht nicht mehr

--- core/src/com/dungeoncrawler/view/View.java
+++ core/src/com/dungeoncrawler/view/View.java
@@ -3,9 +3,12 @@ package com.dungeoncrawler.view;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class View extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -13,6 +16,10 @@ public class View extends ApplicationAdapter {
         Texture t;
         Sprite button;
         Sprite title;
+        Map map;
+        TiledMapRenderer tmr;
+        OrthographicCamera camera;
+
         
 	@Override
 	public void create () {
@@ -28,12 +35,25 @@ public class View extends ApplicationAdapter {
                 title.setY(h - 200);
                 button.setX(wc - (button.getWidth()/2));
                 button.setY(400);
+                
+                map = new Map();
+                tmr = new OrthogonalTiledMapRenderer(map.getMap());
+                
+                camera = new OrthographicCamera(1, h/w);
+
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+                
+                tmr.setView(camera);
+                tmr.render();
+                
+                camera.zoom = 1000f;
+                camera.update();
+                batch.setProjectionMatrix(camera.combined);
                 
 		batch.begin();
                 title.draw(batch);
