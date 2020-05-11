345f7996644e10c19116f985e0223d0d6518389b
GammelJan <jan.ehehalt@gmail.com>
2020-05-11 21:24:16 +0200

    asdf

---
+++ core/src/com/dungeoncrawler/view/EndScreen.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dungeoncrawler.view;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ *
+ * @author Jan
+ */
+public class EndScreen {
+    
+    
+    public void render (SpriteBatch batch, float volume) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+                
+                batch.begin();
+                batch.end();
+	}
+    
+    public int click(int x, int y){     // prueft ob cursor mit button (START) ueberlappt
+            
+            
+            //if(Intersector.overlaps(r, backButtonSprite.getBoundingRectangle())){
+            //    return 5;   //proceed Game
+            //}
+            
+            return -1;
+            
+    }
+}
