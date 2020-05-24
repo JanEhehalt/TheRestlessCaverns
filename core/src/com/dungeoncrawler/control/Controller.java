07b55128a2f740d4187206ef79a6f1146ba4b33a
GammelJan <jan.ehehalt@gmail.com>
2020-05-24 22:34:46 +0200

    jetzt sehr schwer

--- core/src/com/dungeoncrawler/control/Controller.java
+++ core/src/com/dungeoncrawler/control/Controller.java
@@ -73,7 +73,7 @@ public class Controller extends ApplicationAdapter implements InputProcessor{
     @Override
     public void create(){
         
-        checkDoor = false;
+        checkDoor = true;
         checkDie = true;
         
         
